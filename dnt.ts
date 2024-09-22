b4ffc227d87dd4856c5c7e4e0f18fd65034cca3c
Hong Minhee <hong@minhee.org>
2024-09-22 18:00:09 +0900

    Funding information
    [ci skip]

--- dnt.ts
+++ dnt.ts
@@ -36,6 +36,9 @@ await build({
     bugs: {
       url: "https://github.com/dahlia/fedify-redis/issues",
     },
+    funding: [
+      "https://github.com/sponsors/dahlia",
+    ],
   },
   outDir: "./npm",
   entryPoints: [
