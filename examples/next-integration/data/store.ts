fb6b9636e513f8d82f0cb27727c19352d789f55e
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-08-24 11:40:18 +0900

    Remove `any`

--- examples/next-integration/data/store.ts
+++ examples/next-integration/data/store.ts
@@ -1,12 +1,14 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+declare global {
+  var keyPairsStore: Map<string, Array<CryptoKeyPair>>;
+  var relationStore: Map<string, string>;
+}
 
-export const keyPairsStore: Map<string, Array<CryptoKeyPair>> = (
-  globalThis as any
-).keyPairsStore ?? new Map();
-export const relationStore: Map<string, string> =
-  (globalThis as any).relationStore ?? new Map();
+export const keyPairsStore: Map<string, Array<CryptoKeyPair>> =
+  globalThis.keyPairsStore ?? new Map();
+export const relationStore: Map<string, string> = globalThis.relationStore ??
+  new Map();
 
 // this is just a hack to demo nextjs
 // never do this in production, use safe and secure storage
-(globalThis as any).keyPairsStore = keyPairsStore;
-(globalThis as any).relationStore = relationStore;
+globalThis.keyPairsStore = keyPairsStore;
+globalThis.relationStore = relationStore;
