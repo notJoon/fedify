4f288aedbf66f0546db14c431aed84b7bcaf11df
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-08-29 22:51:20 +0900

    Format all files

--- examples/sveltekit-sample/src/routes/users/[identifier]/posts/+page.server.ts
+++ examples/sveltekit-sample/src/routes/users/[identifier]/posts/+page.server.ts
@@ -8,56 +8,56 @@ import fedi from "$lib/federation";
 import { getPosts, getUser } from "$lib/fetch";
 
 const post: Action = async (event) => {
-	const data = await event.request.formData();
-	const content = data.get("content") as string;
-	const identifier = data.get("identifier") as string;
+  const data = await event.request.formData();
+  const content = data.get("content") as string;
+  const identifier = data.get("identifier") as string;
 
-	if (typeof content !== "string" || typeof identifier !== "string") {
-		error(400, "Content and identifier are required");
-	}
-	const ctx = federation.createContext(event.request, undefined);
-	const id = crypto.randomUUID();
-	const attribution = ctx.getActorUri(identifier);
-	const url = new URL(`/users/${identifier}/posts/${id}`, attribution);
-	const post = new Note({
-		id: url,
-		attribution,
-		content,
-		url,
-	});
-	try {
-		postStore.append([post!]);
-		const note = await ctx.getObject(Note, { identifier, id });
-		await ctx.sendActivity(
-			{ identifier },
-			"followers",
-			new Create({
-				id: new URL("#activity", attribution),
-				object: note,
-				actors: note?.attributionIds,
-				tos: note?.toIds,
-				ccs: note?.ccIds,
-			}),
-		);
-		// await getPosts().refresh();
-	} catch {
-		postStore.delete(url);
-	}
-	redirect(303, `/users/${identifier}/posts`);
+  if (typeof content !== "string" || typeof identifier !== "string") {
+    error(400, "Content and identifier are required");
+  }
+  const ctx = federation.createContext(event.request, undefined);
+  const id = crypto.randomUUID();
+  const attribution = ctx.getActorUri(identifier);
+  const url = new URL(`/users/${identifier}/posts/${id}`, attribution);
+  const post = new Note({
+    id: url,
+    attribution,
+    content,
+    url,
+  });
+  try {
+    postStore.append([post!]);
+    const note = await ctx.getObject(Note, { identifier, id });
+    await ctx.sendActivity(
+      { identifier },
+      "followers",
+      new Create({
+        id: new URL("#activity", attribution),
+        object: note,
+        actors: note?.attributionIds,
+        tos: note?.toIds,
+        ccs: note?.ccIds,
+      }),
+    );
+    // await getPosts().refresh();
+  } catch {
+    postStore.delete(url);
+  }
+  redirect(303, `/users/${identifier}/posts`);
 };
 
 export const actions = { post } satisfies Actions;
 
 export const load: PageServerLoad = async ({ request, params }) => {
-	try {
-		const ctx = fedi.createContext(request, undefined);
-		const { identifier } = params;
+  try {
+    const ctx = fedi.createContext(request, undefined);
+    const { identifier } = params;
 
-		const user = await getUser(ctx, identifier);
-		const posts = await getPosts(user);
+    const user = await getUser(ctx, identifier);
+    const posts = await getPosts(user);
 
-		return { user, posts };
-	} catch {
-		error(404, { message: "Not Found" });
-	}
+    return { user, posts };
+  } catch {
+    error(404, { message: "Not Found" });
+  }
 };
