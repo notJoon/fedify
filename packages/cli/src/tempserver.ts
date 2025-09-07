dc52f8c0c7348de5913d5b08ac51d72cae885fab
Jiwon Kwon <kjiwon411@gmail.com>
2025-09-07 17:13:32 +0900

    refactor: use srvx instead Deno API

--- packages/cli/src/tempserver.ts
+++ packages/cli/src/tempserver.ts
@@ -1,82 +1,87 @@
 import { openTunnel } from "@hongminhee/localtunnel";
 import { getLogger } from "@logtape/logtape";
+import { serve } from "srvx";
 
 const logger = getLogger(["fedify", "cli", "tempserver"]);
 
-export interface SpawnTemporaryServerOptions {
+export type SpawnTemporaryServerOptions = {
   noTunnel?: boolean;
-}
+};
 
-export interface TemporaryServer {
+export type TemporaryServer = {
   url: URL;
   close(): Promise<void>;
-}
+};
 
-export function spawnTemporaryServer(
-  handler: Deno.ServeHandler,
+export async function spawnTemporaryServer(
+  fetch: (request: Request) => Promise<Response>,
   options: SpawnTemporaryServerOptions = {},
 ): Promise<TemporaryServer> {
   if (options.noTunnel) {
-    return new Promise((resolve) => {
-      const server = Deno.serve({
-        handler,
-        port: 0,
-        hostname: "::",
-        onListen({ port }) {
-          logger.debug("Temporary server is listening on port {port}.", {
-            port,
-          });
-          resolve({
-            url: new URL(`http://localhost:${port}`),
-            async close() {
-              await server.shutdown();
-            },
-          });
-        },
-      });
-    });
-  }
-  return new Promise((resolve) => {
-    const server = Deno.serve({
-      async handler(request: Request, info: Deno.ServeHandlerInfo) {
-        const url = new URL(request.url);
-        url.protocol = "https:";
-        request = new Request(url, {
-          method: request.method,
-          headers: request.headers,
-          body: request.method === "GET" || request.method === "HEAD"
-            ? null
-            : await request.blob(),
-          referrer: request.referrer,
-          referrerPolicy: request.referrerPolicy,
-          mode: request.mode,
-          credentials: request.credentials,
-          cache: request.cache,
-          redirect: request.redirect,
-          integrity: request.integrity,
-          keepalive: request.keepalive,
-          signal: request.signal,
-        });
-        return await handler(request, info);
-      },
+    const server = serve({
       port: 0,
       hostname: "::",
-      onListen({ port }) {
-        logger.debug("Temporary server is listening on port {port}.", { port });
-        openTunnel({ port }).then((tun) => {
-          logger.debug(
-            "Temporary server is tunneled to {url}.",
-            { url: tun.url.href },
-          );
-          resolve({
-            url: tun.url,
-            async close() {
-              await server.shutdown();
-              await tun.close();
-            },
-          });
-        });
-      },
+      fetch: fetch,
     });
+    await server.ready();
+    const url = new URL(server.url!);
+    const port = url.port;
+    logger.debug("Temporary server is listening on port {port}.", {
+      port: port,
+    });
+
+    return {
+      url: new URL(`http://localhost:${port}`),
+      async close() {
+        await server.close();
+      },
+    };
+  }
+
+  const server = serve({
+    fetch: (request) => {
+      const url = new URL(request.url);
+      url.protocol = "https:";
+      request = new Request(url, {
+        method: request.method,
+        headers: request.headers,
+        body: request.method === "GET" || request.method === "HEAD"
+          ? null
+          : request.body,
+        referrer: request.referrer,
+        referrerPolicy: request.referrerPolicy,
+        mode: request.mode,
+        credentials: request.credentials,
+        cache: request.cache,
+        redirect: request.redirect,
+        integrity: request.integrity,
+        keepalive: request.keepalive,
+        signal: request.signal,
+      });
+
+      return new Response();
+    },
+    port: 0,
+    hostname: "::",
   });
+
+  await server.ready();
+
+  const url = new URL(server.url!);
+  const port = url.port;
+
+  logger.debug("Temporary server is listening on port {port}.", { port });
+  const tun = await openTunnel({ port: parseInt(port) });
+  logger.debug(
+    "Temporary server is tunneled to {url}.",
+    { url: tun.url.href },
+  );
+
+  return {
+    url: tun.url,
+    async close() {
+      await server.close();
+      await tun.close();
+    },
+  };
 }
