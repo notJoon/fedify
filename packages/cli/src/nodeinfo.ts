a98f83833b489b95a26c4d495fd6f5f7412d0f2e
Hyeonseo Kim <dodok8@gmail.com>
2025-09-09 20:20:44 +0900

    feat: add parser to nodeinfo

--- packages/cli/src/nodeinfo.ts
+++ packages/cli/src/nodeinfo.ts
@@ -1,13 +1,22 @@
+import { createJimp } from "@jimp/core";
+import webp from "@jimp/wasm-webp";
 import {
+  argument,
   command as Command,
   constant,
+  flag,
   type InferValue,
   merge,
   message,
   object,
+  option,
+  optional,
+  or,
+  string,
+  withDefault,
 } from "@optique/core";
-import { createJimp } from "@jimp/core";
-import webp from "@jimp/wasm-webp";
+import { url } from "@optique/core/valueparser";
+import { print } from "@optique/run";
 import { defaultFormats, defaultPlugins } from "jimp";
 import { debugOption } from "./globals.ts";
 
@@ -16,13 +25,45 @@ export const Jimp = createJimp({
   plugins: defaultPlugins,
 });
 
+const nodeInfoOption = optional(
+  or(
+    object({
+      raw: flag("-r", "--raw", {
+        description: message`Show NodeInfo document in the raw JSON format`,
+      }),
+    }),
+    object({
+      bestEffort: optional(flag("-b", "--best-effort", {
+        description:
+          message`Parse the NodeInfo document with best effort. If the NodeInfo document is not well-formed, the option will try to parse it as much as possible.`,
+      })),
+      noFavicon: optional(flag("--no-favicon", {
+        description: message`Disable fetching the favicon of the instance`,
+      })),
+      metadata: optional(flag("-m", "--metadata", {
+        description:
+          message`show the extra metadata of the NodeInfo, i.e., the metadata field of the document.`,
+      })),
+    }),
+  ),
+);
+
+const userAgentOption = optional(object({
+  userAgent: option("-u", "--user-agent", string()),
+}));
+
 export const nodeInfoCommand = Command(
   "nodeinfo",
   merge(
     object({
       command: constant("nodeinfo"),
+      host: argument(string({ metavar: "hostname or URL" }), {
+        description: message`Bare hostname or a full URL of the instance`,
+      }),
     }),
     debugOption,
+    nodeInfoOption,
+    userAgentOption,
   ),
   {
     description:
