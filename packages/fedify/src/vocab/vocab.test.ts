f58a7b92091288e078b8c02ecbe7d6b88ef40c60
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-09-24 03:41:46 +0900

    Revert "Replaced `fedify/src/runtime` reference with `@fedify/vocab-runtime` in `fedify/src/vocab`"
    This reverts commit d0023a52a54b1106aefa2c9ae8dbb2e30b05fb3b.

--- packages/fedify/src/vocab/vocab.test.ts
+++ packages/fedify/src/vocab/vocab.test.ts
@@ -1,4 +1,3 @@
-import { decode, LanguageString } from "@fedify/vocab-runtime";
 import {
   assert,
   assertEquals,
@@ -15,6 +14,8 @@ import {
   type TypeSchema,
 } from "../codegen/schema.ts";
 import { areAllScalarTypes } from "../codegen/type.ts";
+import { LanguageString } from "../runtime/langstr.ts";
+import { decode } from "../runtime/multibase/index.ts";
 import { mockDocumentLoader } from "../testing/docloader.ts";
 import { ed25519PublicKey, rsaPublicKey1 } from "../testing/keys.ts";
 import { test } from "../testing/mod.ts";
