077d480bafab85a4e50408bb1df6ac990cf9229c
ChanHaeng Lee <2chanhaeng@gmail.com>
2025-09-29 05:37:58 +0000

    Added pnpm:build-vocab and remove test while building vocab-runtime

--- packages/vocab/runtime/tsdown.config.ts
+++ packages/vocab/runtime/tsdown.config.ts
@@ -1,22 +1,9 @@
-import { glob } from "node:fs/promises";
-import { sep } from "node:path";
 import { defineConfig } from "tsdown";
 
-export default [
-  defineConfig({
-    entry: ["src/mod.ts"],
-    dts: true,
-    format: ["esm", "cjs"],
-    platform: "node",
-    external: [/^node:/],
-  }),
-  defineConfig({
-    entry: [
-      ...(await Array.fromAsync(glob(`src/**/*.test.ts`)))
-        .map((f) => f.replace(sep, "/")),
-    ],
-    dts: true,
-    external: [/^node:/],
-    platform: "node",
-  }),
-];
+export default defineConfig({
+  entry: ["src/mod.ts"],
+  dts: true,
+  format: ["esm", "cjs"],
+  platform: "node",
+  external: [/^node:/],
+});
