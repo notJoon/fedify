520d1baea9f681979d9033b9f98234b3f0475fc6
Hong Minhee <hong@minhee.org>
2025-10-13 16:01:57 +0900

    Reorganize `fedify generate-vocab` command

--- packages/cli/src/generate-vocab/command.ts
+++ packages/cli/src/generate-vocab/command.ts
@@ -1,27 +1,35 @@
 import {
+  argument,
   command,
   constant,
   type InferValue,
   message,
   object,
   option,
+  withDefault,
 } from "@optique/core";
 import { path } from "@optique/run";
 
-const schemaDir = option(
-  "-i",
-  "--input",
-  path({ metavar: "DIR", type: "directory", mustExist: true }),
+const schemaDir = withDefault(
+  option(
+    "-i",
+    "--input",
+    path({ metavar: "DIR", type: "directory", mustExist: true }),
+    { description: message`Directory containing schema files.` },
+  ),
+  ".",
 );
-const generatedPath = option(
-  "-o",
-  "--output",
+const generatedPath = argument(
   path({ metavar: "PATH", type: "file", allowCreate: true }),
+  {
+    description:
+      message`Path to output the generated vocabulary classes.  Should end with ${".ts"} suffix.`,
+  },
 );
 
 const generateVocabCommand = command(
   "generate-vocab",
-  object({
+  object("Generation options", {
     command: constant("generate-vocab"),
     schemaDir,
     generatedPath,
