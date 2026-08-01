f3e243407343cff4fdcbc3ee3439858a135e6cd6
Seoa Yoon <me@seoa.dev>
2026-08-01 15:45:01 +0900

    Add non-federation request delegation test in fedify/next

--- packages/next/src/index.test.ts
+++ packages/next/src/index.test.ts
@@ -1,6 +1,11 @@
+import {
+  createFederation,
+  InProcessMessageQueue,
+  MemoryKvStore,
+} from "@fedify/fedify";
 import { test } from "@fedify/fixture";
 import { strict as assert } from "node:assert";
-import { isFederationRequest, isNodeInfoRequest } from "./index.ts";
+import { fedifyWith, isFederationRequest, isNodeInfoRequest } from "./index.ts";
 
 test("Accept header detection", () => {
   const request = new Request("https://example.com/", {
@@ -33,4 +38,39 @@ test("NodeInfo route detection", () => {
 
   assert.strictEqual(isNodeInfoRequest(request1), true);
   assert.strictEqual(isNodeInfoRequest(request2), true);
+});
+
+test("Non-federation request delegation", async () => {
+  const federation = createFederation({
+    kv: new MemoryKvStore(),
+    queue: new InProcessMessageQueue(),
+  });
+  const customMiddleware = (request: Request) => {
+    if (request.url === "https://example.com/test") {
+      return new Response("Custom middleware response");
+    }
+    return new Response("Default response");
+  };
+  const middleware = fedifyWith(federation)(customMiddleware);
+
+  const request = new Request("https://example.com/", {
+    headers: {
+      Accept: "text/html",
+    },
+  });
+
+  const request2 = new Request("https://example.com/test", {
+    headers: {
+      Accept: "text/html",
+    },
+  });
+
+  assert.strictEqual(isFederationRequest(request), false);
+  assert.strictEqual(isFederationRequest(request2), false);
+
+  const response1 = await middleware(request);
+  const response2 = await middleware(request2);
+
+  assert.strictEqual(await response1.text(), "Default response");
+  assert.strictEqual(await response2.text(), "Custom middleware response");
 });
